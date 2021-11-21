d6f061f6481a836d52a9a6c162f1bc4423b4283c
Bob F <bobfdev@gmail.com>
2021-11-20 22:37:02 -0600

    added FeedbackStats file to components folder

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
 	import FeedbackList from './components/FeedbackList.svelte';
+	import FeedbackStats from './components/FeedbackStats.svelte';
 
 	let feedback = [
 		{
@@ -19,15 +20,18 @@
 		},
 	]
 
+$: count = feedback.length
+$: average = feedback.reduce((a, {rating}) => a + rating, 0) / feedback.length
+
 const deleteFeedback = (e) => {
-	const itemId = e.details
+	const itemId = e.detail
 	feedback = feedback.filter((item) => item.id !== itemId)
 }
 </script>
 
 <main class="container">
-	<FeedbackList 
-		feedback={feedback} 
+	<FeedbackStats {count} {average} />
+	<FeedbackList {feedback} 
 		on:delete-feedback={deleteFeedback}
 	/>
 </main>
